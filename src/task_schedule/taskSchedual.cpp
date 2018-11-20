1819b85357a718ec2e1624d34c5e507259947a89
yangdong <yangdong@boostiv.com>
2018-11-20 15:28:18 +0800

    modify task_schedual

--- src/task_schedule/taskSchedual.cpp
+++ src/task_schedule/taskSchedual.cpp
@@ -87,47 +87,47 @@ void CTaskSchedual::do_get_record_info(message_base *message)
 
 }
 
-void* CTaskSchedual::real_paly_thread_proc(void*)
-{
-	// while(true)
-	// {
-	// 	PLAY_MESSAGE_T message = CMyFifo<PLAY_MESSAGE_T>::get_instance()->pop();
-	// 	if (strlen(message.user_id) > 0)
-	// 	{
-	// 		printf("TaskSchedual receive start real play command from fifo\n");
-	// 		printf("message.device_id = %s\n", message.device_id);
-	// 		printf("message.realm = %s\n", message.realm);
-	// 		printf("message.receive_ip = %s\n", message.receive_ip);
-	// 		printf("message.receive_port = %d\n", message.receive_port);
-	// 		printf("message.user_id = %s\n", message.user_id);
+// void* CTaskSchedual::real_paly_thread_proc(void*)
+// {
+// 	// while(true)
+// 	// {
+// 	// 	PLAY_MESSAGE_T message = CMyFifo<PLAY_MESSAGE_T>::get_instance()->pop();
+// 	// 	if (strlen(message.user_id) > 0)
+// 	// 	{
+// 	// 		printf("TaskSchedual receive start real play command from fifo\n");
+// 	// 		printf("message.device_id = %s\n", message.device_id);
+// 	// 		printf("message.realm = %s\n", message.realm);
+// 	// 		printf("message.receive_ip = %s\n", message.receive_ip);
+// 	// 		printf("message.receive_port = %d\n", message.receive_port);
+// 	// 		printf("message.user_id = %s\n", message.user_id);
 			
-	// 		if(real_time_paly == message.play_action)
-	// 		{
-	// 				resip::BasicClientUserAgent::getInstance()->doInvite(message.user_id, message.device_id, message.realm,
-	// 							message.receive_ip, message.receive_port);
-	// 				printf("TaskSchedual use sipua send invite sip message\n");
-	// 		}
-	// 		else if(real_time_stop == message.play_action)
-	// 		{
-	// 				resip::BasicClientUserAgent::getInstance()->doBye(message.user_id);
-	// 				printf("TaskSchedual use sipua send bye sip message\n");
-	// 		}
-	// 	}
-	// 	sleep(1);
-	// }
-}
-void* CTaskSchedual::review_thread_proc(void*)
-{
+// 	// 		if(real_time_paly == message.play_action)
+// 	// 		{
+// 	// 				resip::BasicClientUserAgent::getInstance()->doInvite(message.user_id, message.device_id, message.realm,
+// 	// 							message.receive_ip, message.receive_port);
+// 	// 				printf("TaskSchedual use sipua send invite sip message\n");
+// 	// 		}
+// 	// 		else if(real_time_stop == message.play_action)
+// 	// 		{
+// 	// 				resip::BasicClientUserAgent::getInstance()->doBye(message.user_id);
+// 	// 				printf("TaskSchedual use sipua send bye sip message\n");
+// 	// 		}
+// 	// 	}
+// 	// 	sleep(1);
+// 	// }
+// }
+// void* CTaskSchedual::review_thread_proc(void*)
+// {
 
-}
-void* CTaskSchedual::ptz_control_thread_proc(void*)
-{
+// }
+// void* CTaskSchedual::ptz_control_thread_proc(void*)
+// {
 
-}
-void* CTaskSchedual::deal_directory_info_thread_proc(void*)
-{
+// }
+// void* CTaskSchedual::deal_directory_info_thread_proc(void*)
+// {
 
-}
+// }
 
 int CTaskSchedual::start()
 {
--- src/task_schedule/taskSchedual.h
+++ src/task_schedule/taskSchedual.h
@@ -11,10 +11,10 @@ public:
     int start();
     int stop();
     static void* dispatch_message_thread_proc(void *);
-    static void* real_paly_thread_proc(void*);
-    static void* review_thread_proc(void*);
-    static void* ptz_control_thread_proc(void*);
-    static void* deal_directory_info_thread_proc(void*);
+    // static void* real_paly_thread_proc(void*);
+    // static void* review_thread_proc(void*);
+    // static void* ptz_control_thread_proc(void*);
+    // static void* deal_directory_info_thread_proc(void*);
     bool dispatch_message_thread_proc_is_running();
 
     void diapatech_message(message_base* message);
@@ -34,10 +34,10 @@ public:
     pthread_t m_dispatch_message_pthread_id;
     pthread_t m_respose_deal_proc_pthread_id;
 
-    pthread_t m_real_paly_thread_id;
-    pthread_t m_review_thread_id;
-    pthread_t m_ptz_control_thread_id;
-    pthread_t m_deal_directory_info_thread_id;
+    // pthread_t m_real_paly_thread_id;
+    // pthread_t m_review_thread_id;
+    // pthread_t m_ptz_control_thread_id;
+    // pthread_t m_deal_directory_info_thread_id;
 };
 
 #endif // !__TASKSCHEDUAL_H__
