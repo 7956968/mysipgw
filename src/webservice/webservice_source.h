87c6b8da2736957b95a9deb273b9d81f29067f9c
yangdong <yangdong@boostiv.com>
2018-11-21 11:10:33 +0800

    add webservice_source.h

--- src/webservice/webservice_source.h
+++ src/webservice/webservice_source.h
@@ -12,18 +12,18 @@
 //gsoap ns2  service method-action:        add ""
 //gsoap ns2  service method-output-action: add Response
 
-int ns2__login(char* user_id, char* password, int& result);
-int ns2__logout(char* user_id, char* password, int& result);
-int ns2__keepalive(char* user_id, char* password, int& result);
+int ns2__login(char* user_id, char* password, char** result);
+int ns2__logout(char *user_id, char *password, char **result);
+int ns2__keepalive(char *user_id, char *password, char **result);
 
 int ns2__get_directory_info(char* target_dev_id, char* target_realm, char** directory_info_buf_out);
-int ns2__ptz_control(char* target_dev_id, char* target_realm, int ptz, int ptz_speed, int& result);
+int ns2__ptz_control(char *target_dev_id, char *target_realm, int ptz, int ptz_speed, char **result);
 
-int ns2__start_real_play(char* user_id, char* target_dev_id, char* target_realm, char* media_recv_ip, int media_recv_port, int& result);
-int ns2__stop_real_play(char* user_id, char* target_dev_id, char* target_realm, char* media_recv_ip, int media_recv_port, int& result);
+int ns2__start_real_play(char *user_id, char *target_dev_id, char *target_realm, char *media_recv_ip, int media_recv_port, char **result);
+int ns2__stop_real_play(char *user_id, char *target_dev_id, char *target_realm, char *media_recv_ip, int media_recv_port, char **result);
 
-int ns2__start_play_back(char* user_id, char* target_dev_id, char* target_realm, char* media_recv_ip, int media_recv_port, char* start_time, char* end_time, int& result);
-int ns2__stop_play_back(char* user_id, char* target_dev_id, char* target_realm, char* media_recv_ip, int media_recv_port, char* start_time, char* end_time, int& result);
+int ns2__start_play_back(char *user_id, char *target_dev_id, char *target_realm, char *media_recv_ip, int media_recv_port, char *start_time, char *end_time, char **result);
+int ns2__stop_play_back(char *user_id, char *target_dev_id, char *target_realm, char *media_recv_ip, int media_recv_port, char *start_time, char *end_time, char **result);
 
-int ns2__pause_play_back(char* user_id, char* target_dev_id, char* target_realm, char* media_recv_ip, int media_recv_port, int& result);
-int ns2__restart_play_back(char* user_id, char* target_dev_id, char* target_realm, char* media_recv_ip, int media_recv_port, int& result);
+int ns2__pause_play_back(char *user_id, char *target_dev_id, char *target_realm, char *media_recv_ip, int media_recv_port, char **result);
+int ns2__restart_play_back(char *user_id, char *target_dev_id, char *target_realm, char *media_recv_ip, int media_recv_port, char **result);
