bbb0e82cc9dd3b7803ff18ef5114703707c785f1
yangdong <yangdong@boostiv.com>
2018-11-22 11:51:09 +0800

    using 192.168.2.126

--- src/webservice/bst_webservice.cpp
+++ src/webservice/bst_webservice.cpp
@@ -12,7 +12,7 @@ int http_get(struct soap *soap)
     FILE *fd;
     char *s = strrchr(soap->path, '/'); // soap->path has the URL path (soap->endpoint has the full URL)
     char *respose_message = "method not supported\n if you want get wsdl file. please input:"
-                            "\n\"http://192.168.2.128:9800/SipGwServer?wsdl\" "
+                            "\n\"http://bst_sipgw_server:9800/SipGwServer?wsdl\" "
                             "\nin your web browser.";
     if (!s || strcmp(s, "/SipGwServer?wsdl"))
     {
