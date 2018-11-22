24c017347a86a013e4382924165434e781eaf342
yangdong <yangdong@boostiv.com>
2018-11-22 11:34:17 +0800

    add webservice.

--- src/webservice/bst_webservice.h
+++ src/webservice/bst_webservice.h
@@ -23,25 +23,25 @@ class CSipgwSvr : public sipgwService
     //inherit from parent class
 
     /// Web service operation 'login' (returns SOAP_OK or error code)
-    virtual int login(char *user_id, char *password, char **result) SOAP_PURE_VIRTUAL;
+    virtual int login(char *user_id, char *password, char **result) ;
     /// Web service operation 'logout' (returns SOAP_OK or error code)
-    virtual int logout(char *user_id, char *password, char **result) SOAP_PURE_VIRTUAL;
+    virtual int logout(char *user_id, char *password, char **result) ;
     /// Web service operation 'keepalive' (returns SOAP_OK or error code)
-    virtual int keepalive(char *user_id, char *password, char **result) SOAP_PURE_VIRTUAL;
+    virtual int keepalive(char *user_id, char *password, char **result) ;
     /// Web service operation 'get-directory-info' (returns SOAP_OK or error code)
-    virtual int get_directory_info(char *target_dev_id, char *target_realm, char **directory_info_buf_out) SOAP_PURE_VIRTUAL;
+    virtual int get_directory_info(char *target_dev_id, char *target_realm, char **directory_info_buf_out) ;
     /// Web service operation 'ptz-control' (returns SOAP_OK or error code)
-    virtual int ptz_control(char *target_dev_id, char *target_realm, int ptz, int ptz_speed, char **result) SOAP_PURE_VIRTUAL;
+    virtual int ptz_control(char *target_dev_id, char *target_realm, int ptz, int ptz_speed, char **result) ;
     /// Web service operation 'start-real-play' (returns SOAP_OK or error code)
-    virtual int start_real_play(char *user_id, char *target_dev_id, char *target_realm, char *media_recv_ip, int media_recv_port, char **call_id) SOAP_PURE_VIRTUAL;
+    virtual int start_real_play(char *user_id, char *target_dev_id, char *target_realm, char *media_recv_ip, int media_recv_port, char **call_id) ;
     /// Web service operation 'stop-real-play' (returns SOAP_OK or error code)
-    virtual int stop_real_play(char *user_id, char *call_id, char **result) SOAP_PURE_VIRTUAL;
+    virtual int stop_real_play(char *user_id, char *call_id, char **result) ;
     /// Web service operation 'start-play-back' (returns SOAP_OK or error code)
-    virtual int start_play_back(char *user_id, char *target_dev_id, char *target_realm, char *media_recv_ip, int media_recv_port, char *start_time, char *end_time, char **call_id) SOAP_PURE_VIRTUAL;
+    virtual int start_play_back(char *user_id, char *target_dev_id, char *target_realm, char *media_recv_ip, int media_recv_port, char *start_time, char *end_time, char **call_id) ;
     /// Web service operation 'stop-play-back' (returns SOAP_OK or error code)
-    virtual int stop_play_back(char *user_id, char *call_id, char **result) SOAP_PURE_VIRTUAL;
+    virtual int stop_play_back(char *user_id, char *call_id, char **result) ;
     /// Web service operation 'call-lookup' (returns SOAP_OK or error code)
-    virtual int call_lookup(char *user_id, char **result) SOAP_PURE_VIRTUAL;
+    virtual int call_lookup(char *user_id, char **result) ;
 };
 
 #endif
